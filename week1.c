ca953f225029f2b6874500f5d7154a18f254fbfb
Kat <karacav@msn.com>
2022-03-06 22:01:21 -0800

    starting #2

--- week1.c
+++ week1.c
@@ -8,3 +8,23 @@ int main(void)
   // print hello, user_name
     printf("hello, %s\n", user_name);
 }
+
+// #2
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int user_number = get_int("input a number between 1 and 8 ");
+    while(user_number < 1 || user_number > 8) {
+        user_number = get_int("please input a number between 1 and 8");
+    }
+    for(int i = 0; i < user_number; i++) {
+       // printf("#");
+        for(int j = 0; j < user_number; j++) {
+              printf("%i ", i);
+            printf("#");
+        }
+         printf("\n");
+    }
+}
