2e48fe0ea26c9b38956b74f937de7e17071d493d
Kat <karacav@msn.com>
2022-03-10 11:11:38 -0800

    Week One HW 1- Mario Solution

--- week1.c
+++ week1.c
@@ -15,21 +15,29 @@ int main(void)
 
 int main(void)
 {
-    int user_number = get_int("input a number between 1 and 8 ");
-    while(user_number < 1 || user_number > 8) {
-        user_number = get_int("please input a number between 1 and 8");
-    }
-    for(int i = 0; i < user_number; i++) {
-       // printf("#");
-        for(int j = user_number; j > i; j--) {
-       //     printf("%i ", i);
-            printf("#");
-        }
-         printf("  ");
-        for(int j = 0; j <= i; j++) {
-            printf("#");
-        }
 
-         printf("\n");
+    int user_number = get_int("Pick a pyramid height between 1 and 8: ");
+
+    while(user_number < 1 || user_number > 8) {
+        user_number = get_int("Pyramids can only be 1 to 8 blocks high! Try again: ");
+    }
+    for(int i = 1; i <= user_number; i++) {
+
+        for(int j = user_number; j > i; j--) {
+            printf(" ");
+        }
+        for(int j = 1; j <= i; j++) {
+            printf("#");
+        }
+        printf("  ");
+        for(int j = 1; j <= i; j++) {
+            printf("#");
+        }
+        printf("\n");
     }
 }
+// User Input = 3
+// Result:
+//   #  #
+//  ##  ##
+// ###  ###
